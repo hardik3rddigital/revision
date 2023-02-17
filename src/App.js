3a122e79085bb14b112c7d363decdf3440ffb6ca
hardik3rddigital <hardik.s@3rddigital.com>
2023-02-17 12:16:45 +0530

    6. Event Handling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Props from "./Props";
+import Props from "./Components/Props";
+import EventHandle from "./Components/EventHandle";
 
 function App() {
   return (
     <div className="App">
-      <div className="container mt-5 shadow p-3">
+      <div className="container mt-5 shadow p-3 border">
         <Props name="Hardik Patel"><h5 className="m-0">I Am Props Children</h5></Props>
+      </div>
+
+      <div className="container mt-5 shadow p-3 border">
+        <EventHandle />
       </div>
     </div>
   );
---
+++ src/Components/EventHandle.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState    } from "react";
+
+const EventHandle = () => {
+  const [classname, setclassname] = useState('');
+  const clickEventHandle = (event) => {
+    const getClassName = event.target.className;
+    console.log(event.target.className);
+    setclassname(getClassName);
+  };
+
+  return (
+    <>
+      <h5>Hello I am EventHandle Components</h5>
+      <button className="btn btn-primary mb-2" onClick={clickEventHandle}>
+        Click Event
+      </button>
+      <h5 className="mb-0">Button Classname : {classname}</h5>
+    </>
+  );
+};
+
+export default EventHandle;
