c036a87bf5adaf0c360ecb9aa315cde777a8a91a
hardik3rddigital <hardik.s@3rddigital.com>
2023-02-20 17:17:54 +0530

    useHook render condition error

--- src/Components/Hooks/useEffect/useEffectHook.js
+++ src/Components/Hooks/useEffect/useEffectHook.js
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react'
 
 function UseEffectHook() {
   const [count,setCount] = useState(0);
+  const [name,setName] = useState('');
 
   useEffect(()=>{
     document.title = `You Clicked ${count} times`;
+    console.log(`You Clicked ${count} times`)
   })
     
   return (
     <>
         <h5>Hello I am useEffect Hook Components</h5>
         <h5>Counter : {count} </h5>
+        <input type='text' className="form-control mb-3" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
         <button className='btn btn-primary' onClick={()=> setCount(count + 1)}>Increment</button>
     </>
   )
