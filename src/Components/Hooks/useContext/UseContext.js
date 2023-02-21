e98f21c12c6c3afb4a4454e44d9488fc5e143010
hardik3rddigital <hardik.s@3rddigital.com>
2023-02-21 12:08:54 +0530

    Multiple useContext

--- src/Components/Hooks/useContext/UseContext.js
+++ src/Components/Hooks/useContext/UseContext.js
@@ -2,20 +2,26 @@ import React from "react";
 import UseContextA from "./UseContextA";
 
 const UserContextUseContext = React.createContext();
+const MultipleContextUseContext = React.createContext();
 
-const CustomData = 
-  {
-    name: "Hardk Patel",
-    component: "UseContext",
-  }
+const CustomData = {
+  name: "Hardk Patel",
+  component: "UseContext",
+};
 
+const MultipleCustomData = {
+  nickname: "H D Satapara",
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+};
 
 function UseContext() {
   return (
     <>
       <h5>Hello I am UseContext Components</h5>
       <UserContextUseContext.Provider value={CustomData}>
-        <UseContextA />
+        <MultipleContextUseContext.Provider value={MultipleCustomData}>
+          <UseContextA />
+        </MultipleContextUseContext.Provider>
       </UserContextUseContext.Provider>
     </>
   );
@@ -23,4 +29,4 @@ function UseContext() {
 
 export default UseContext;
 
-export {UserContextUseContext}
+export { UserContextUseContext,MultipleContextUseContext };
--- src/Components/Hooks/useContext/UseContextB.js
+++ src/Components/Hooks/useContext/UseContextB.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import { UserContextUseContext } from './UseContext'
+import React from "react";
+import { UserContextUseContext, MultipleContextUseContext } from "./UseContext";
 
 function UseContextB() {
   return (
     <>
-      <UserContextUseContext>
-        {
-          userData => {
-            return <>
-              <h5 className='list-group-item'>My Name is : <b>{userData.name}</b> & My Componet Name is : <b>{userData.component}</b> </h5>
+      <UserContextUseContext.Consumer>
+        {(userData) => {
+          return (
+            <>
+              <MultipleContextUseContext.Consumer>
+                {(multipleData) => {
+                  return (
+                    <>
+                      <h5 className="list-group-item">
+                        My Name is : <b className="text-decoration-underline fst-italic">{userData.name}</b> & My Componet Name
+                        is : <b>{userData.component}</b>
+                      </h5>
+                      <h5 className="list-group-item">
+                        My Nickname is : <b className="text-decoration-underline">{multipleData.nickname}</b> & My Componet description
+                        is : <b>{multipleData.description}</b>
+                      </h5>
+                    </>
+                  );
+                }}
+              </MultipleContextUseContext.Consumer>
             </>
-          }
-        }
-      </UserContextUseContext>
+          );
+        }}
+      </UserContextUseContext.Consumer>
     </>
-  )
+  );
 }
 
-export default UseContextB
+export default UseContextB;
